18798cb669683d982d04142ebc9340b2bc1c3bb3
rvalandas <rvalandas@gmail.com>
2018-01-06 17:31:42 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,7 @@
 library(plyr)
 
-# Step 1
-# Merge the training and test sets to create one data set
-###############################################################################
+#1
+# Merge training and test sets 
 
 x_train <- read.table("train/X_train.txt")
 y_train <- read.table("train/y_train.txt")
@@ -21,9 +20,8 @@ y_data <- rbind(y_train, y_test)
 # create 'subject' data set
 subject_data <- rbind(subject_train, subject_test)
 
-# Step 2
+#2
 # Extract only the measurements on the mean and standard deviation for each measurement
-###############################################################################
 
 features <- read.table("features.txt")
 
@@ -36,9 +34,9 @@ x_data <- x_data[, mean_and_std_features]
 # correct the column names
 names(x_data) <- features[mean_and_std_features, 2]
 
-# Step 3
+#3
 # Use descriptive activity names to name the activities in the data set
-###############################################################################
+
 
 activities <- read.table("activity_labels.txt")
 
@@ -48,9 +46,8 @@ y_data[, 1] <- activities[y_data[, 1], 2]
 # correct column name
 names(y_data) <- "activity"
 
-# Step 4
+#4
 # Appropriately label the data set with descriptive variable names
-###############################################################################
 
 # correct column name
 names(subject_data) <- "subject"
@@ -58,10 +55,9 @@ names(subject_data) <- "subject"
 # bind all the data in a single data set
 all_data <- cbind(x_data, y_data, subject_data)
 
-# Step 5
+#5
 # Create a second, independent tidy data set with the average of each variable
 # for each activity and each subject
-###############################################################################
 
 # 66 <- 68 columns but last two (activity & subject)
 averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
